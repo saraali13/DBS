caaca854759d5e5cb455f0f27f47b5c703cb0e81
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-09-06 22:49:30 +0500

    Update Q.sql

--- Lab 3 /In_Lab /Q.sql
+++ Lab 3 /In_Lab /Q.sql
@@ -1,4 +1,3 @@
-
 create table departments(
 dep_id int primary key,
 dep_name varchar(15) not null
@@ -21,7 +20,7 @@ insert into departments(dep_id,dep_name) values (3,'Management');
 insert into employee(emp_id,full_name,salary,dep_id) values (1,'Ali Haider',5000,2);
 alter table departments add constraint unique_dep_name unique(dep_name);
 alter table employee drop column dep_id;
-alter table employee add (dep_id int , foreign key(dep_id) references departments(dep_id));
+alter table employee add (dep_id int ,CONSTRAINT fk_emp_dept foreign key(dep_id) references departments(dep_id));
 alter table employee add bonus number(6,2) default 1000;
 alter table employee add (city varchar(10) default 'Karachi',age int constraint check_age check(age>18));
 insert into employee(emp_id,full_name,salary,dep_id,bonus,city,age) values (4,'Ali Akber',70000,1,2359.12,'Lahore',22);
