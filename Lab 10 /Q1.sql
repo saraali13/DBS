0f1870e2cd672a5f664b673f1ff9e60ce37b323e
S. Sara Ali Zaidi <142868034+saraali13@users.noreply.github.com>
2025-12-08 19:49:41 +0500

    Update Q1.sql

--- Lab 10 /Q1.sql
+++ Lab 10 /Q1.sql
@@ -1,1 +1,33 @@
 
+CREATE TABLE bank_accounts (
+    account_no   NUMBER PRIMARY KEY,
+    holder_name  VARCHAR2(50),
+    balance      NUMBER
+);
+
+INSERT INTO bank_accounts VALUES (101, 'Ali', 50000);
+INSERT INTO bank_accounts VALUES (102, 'Sara', 30000);
+INSERT INTO bank_accounts VALUES (103, 'Abbas', 20000);
+
+COMMIT;
+
+SELECT * FROM bank_accounts;
+
+-- Deduct 5000 from account A (101)
+UPDATE bank_accounts
+SET balance = balance - 5000
+WHERE account_no = 101;
+
+-- Credit 5000 to account B (102)
+UPDATE bank_accounts
+SET balance = balance + 5000
+WHERE account_no = 102;
+
+-- Mistaken Update (C)
+UPDATE bank_accounts
+SET balance = balance + 999999  -- mistake
+WHERE account_no = 103;
+
+ROLLBACK;
+
+SELECT * FROM bank_accounts;
